5b96dc4a13ca5d82347cfa9518cdea0f02f69931
JakobLarsson <jakob.larsson@live.se>
2021-03-21 14:12:45 +0100

    Add project files.

---
+++ ASP_inlamning1/Model/Attendee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_inlamning1.Model
+{
+    public class Attendee
+    {
+
+        public int ID { get; set; }
+
+        public string name { get; set; }
+
+        public string email { get; set; }
+
+        public int phoneNumber { get; set; }
+    }
+}
---
+++ ASP_inlamning1/Model/Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_inlamning1.Model
+{
+    public class Event
+    {
+        public int ID { get; set; }
+
+        public string title{ get; set; }
+
+        public int organizer_id { get; set; }
+
+        public string description { get; set; }
+        
+        public string place { get; set; }
+
+        public string address { get; set; }
+
+        public DateTime date { get; set; }
+
+        public int spots_avalible { get; set; }
+
+    }
+}
